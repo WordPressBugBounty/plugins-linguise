8e428d80e7d5e56c9a61d6ce0086fd526ca4f0ae
Sean Murphy <sean@iamseanmurphy.com>
2025-06-18 05:18:47 +0000

    2.1.44

--- linguise/src/constants.php
+++ linguise/src/constants.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined('LINGUISE_SCRIPT_TRANSLATION_VERSION')) {
-    define('LINGUISE_SCRIPT_TRANSLATION_VERSION', 'wordpress_plugin/2.1.43');
+    define('LINGUISE_SCRIPT_TRANSLATION_VERSION', 'wordpress_plugin/2.1.44');
 }
 
 if (!defined('LINGUISE_VERSION')) {
-    define('LINGUISE_VERSION', '2.1.43');
+    define('LINGUISE_VERSION', '2.1.44');
 }
--- linguise/vendor/composer/installed.php
+++ linguise/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'linguise/wordpress-plugin',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '728137b67c4ef809d446e223bb0c029d75563ab1',
+        'reference' => '4aa31b985982b5e79597802cf3b0e0c4d17d76ab',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -31,7 +31,7 @@
         'linguise/wordpress-plugin' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '728137b67c4ef809d446e223bb0c029d75563ab1',
+            'reference' => '4aa31b985982b5e79597802cf3b0e0c4d17d76ab',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
