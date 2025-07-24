f9c013ab539f223f0187f181f028c2d9e0e0da38
Sean Murphy <sean@iamseanmurphy.com>
2025-07-24 06:18:53 +0000

    2.1.56

--- linguise/src/constants.php
+++ linguise/src/constants.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined('LINGUISE_SCRIPT_TRANSLATION_VERSION')) {
-    define('LINGUISE_SCRIPT_TRANSLATION_VERSION', 'wordpress_plugin/2.1.55');
+    define('LINGUISE_SCRIPT_TRANSLATION_VERSION', 'wordpress_plugin/2.1.56');
 }
 
 if (!defined('LINGUISE_VERSION')) {
-    define('LINGUISE_VERSION', '2.1.55');
+    define('LINGUISE_VERSION', '2.1.56');
 }
